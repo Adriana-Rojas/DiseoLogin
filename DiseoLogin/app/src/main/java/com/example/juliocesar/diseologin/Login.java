d55961b7a2ecb93b82dba4a00cd7ad999853a1dc
adriaa <apatriciarojas@ucundinamarca.edu.co>
2020-07-07 04:42:14 -0500

    amor le funciona a mi no

--- DiseoLogin/app/src/main/java/com/example/juliocesar/diseologin/Login.java
+++ DiseoLogin/app/src/main/java/com/example/juliocesar/diseologin/Login.java
@@ -1,108 +1,88 @@
 package com.example.juliocesar.diseologin;
 
-import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.juliocesar.diseologin.database.DataHelper;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class Login extends AppCompatActivity {
 
-    Button btn_ir_crearCuenta,iniciar_sesion;
-    EditText et_correo,et_constraseña;
-    String bdcorreo,bdcontraseña,correooo,Constraseñaaa;
-    DataHelper dataHelper;
-    protected Cursor cursor;
-
+    EditText edtUsuario, edtPassword;
+    Button btnLogin;
+    String usuario,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        dataHelper = new DataHelper(this);
+        edtUsuario = findViewById(R.id.et_correo);
+        edtPassword = findViewById(R.id.et_contrasena);
+        btnLogin = findViewById(R.id.iniciar_sesion);
 
-        btn_ir_crearCuenta=findViewById(R.id.btn_ir_crearCuenta);
-        iniciar_sesion=(Button) findViewById(R.id.iniciar_sesion);
-        et_correo=(EditText) findViewById(R.id.et_correo);
-        et_constraseña=(EditText) findViewById(R.id.et_contrasena);
-
-        DataHelper admin = new DataHelper(this);
-        final SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
-
-
-        btn_ir_crearCuenta.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this,AddUsuario.class));
-                finish();
-            }
-        });
-
-
-        iniciar_sesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(et_correo.length()== 0  ){
-                    et_correo.setError("Correo es obligatorio");
-                    et_correo.requestFocus();
-                }if( et_constraseña.length()== 0 ){
-                    et_constraseña.setError("Contraseña es obligatorio");
-                    et_constraseña.requestFocus();
-                }else {
-                    correooo=et_correo.getText().toString();
-                    Constraseñaaa=et_constraseña.getText().toString();
-                    if (correooo.equals("admin") && Constraseñaaa.equals("admin")) {
-                        startActivity(new Intent(Login.this, MenuAdmin.class));
-                        finish();
-                    }else {
-                        boolean bandera  =false;
-                        et_correo = (EditText) findViewById(R.id.et_correo);
-                        String name = et_correo.getText().toString();
-
-                        Intent mIntent = getIntent();
-                        et_correo.setText(mIntent.getStringExtra("correo"));
-
-                        SQLiteDatabase db = dataHelper.getReadableDatabase();
-                        Bundle bundle = getIntent().getExtras();
-                        cursor = db.rawQuery("SELECT * FROM tb_evaluador WHERE correo = '" + name + "'", null);
-                        cursor.moveToFirst();
-                        if (cursor.moveToFirst() == true) {
-                            bandera=true;
-                            bdcorreo = (cursor.getString(3).toString());
-                            bdcontraseña = (cursor.getString(4).toString());
-                            if (correooo.equals(bdcorreo) && Constraseñaaa.equals(bdcontraseña)) {
-                                startActivity(new Intent(Login.this, MenuEvaluador.class));
-                                finish();
-                            } else {
-                                Toast.makeText(Login.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
-                            }
-                        }else if(bandera==false) {
-                            cursor = db.rawQuery("SELECT * FROM tb_blog WHERE correo = '" + name + "'", null);
-                            cursor.moveToFirst();
-                            if (cursor.moveToFirst() == true) {
-                                bdcorreo = (cursor.getString(3).toString());
-                                bdcontraseña = (cursor.getString(4).toString());
-                                if (correooo.equals(bdcorreo) && Constraseñaaa.equals(bdcontraseña)) {
-                                    startActivity(new Intent(Login.this, MenuUsuario.class));
-                                    finish();
-                                } else {
-                                    Toast.makeText(Login.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
-                                }
-                            } else {
-                                Toast.makeText(Login.this, "Correo no existe", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
+                usuario=edtUsuario.getText().toString();
+                password=edtPassword.getText().toString();
+                if(!usuario.isEmpty()&&!password.isEmpty()){
+                    validarUsuario("http://192.168.1.101/developeru/validar_usuario.php");
+                }else{
+                    Toast.makeText(Login.this,"no se permite",Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
+
+
     }
 
+    private void validarUsuario(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()){
+                    Intent intent = new Intent(getApplicationContext(),MenuAdmin.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this, "Email or Password Invalid", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("usuario", edtUsuario.getText().toString());
+                parametros.put("password", edtPassword.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
 }
