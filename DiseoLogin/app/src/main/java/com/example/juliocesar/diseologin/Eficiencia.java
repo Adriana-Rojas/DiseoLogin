1b2dd49647671a6748a8c4a89045c8d1829e3613
adriaa <apatriciarojas@ucundinamarca.edu.co>
2020-07-25 03:47:09 -0500

    mas

--- DiseoLogin/app/src/main/java/com/example/juliocesar/diseologin/Eficiencia.java
+++ DiseoLogin/app/src/main/java/com/example/juliocesar/diseologin/Eficiencia.java
@@ -6,14 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Eficiencia extends AppCompatActivity {
     Button siguiente;
+    private Spinner  tiempoinicio,tiemporespuesta,numerodeevaluaciones,calculartiemporespuesta,consumoram,
+            consumomedram,consumomaxram,calculoram,consumocpu,consumomedcpu,consumomaxcpu,calculocpu,canticonsumida,
+            consumomedbateria,calculobateria,esfuerzo,completartarea,calculoesfuerzo,efectividadrelativatarea,costototal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eficiencia);
         siguiente = findViewById(R.id.eficiencia);
+        tiempoinicio=(Spinner) findViewById(R.id.spinneraplicacion);
+        tiemporespuesta=(Spinner) findViewById(R.id.spinneraplicacion);
+        numerodeevaluaciones=(Spinner) findViewById(R.id.spinneraplicacion);
+        calculartiemporespuesta=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumoram=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumomedram=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumomaxram=(Spinner) findViewById(R.id.spinneraplicacion);
+        calculoram=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumocpu=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumomedcpu=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumomaxcpu=(Spinner) findViewById(R.id.spinneraplicacion);
+        calculocpu=(Spinner) findViewById(R.id.spinneraplicacion);
+        canticonsumida=(Spinner) findViewById(R.id.spinneraplicacion);
+        consumomedbateria=(Spinner) findViewById(R.id.spinneraplicacion);
+        calculobateria=(Spinner) findViewById(R.id.spinneraplicacion);
+        esfuerzo=(Spinner) findViewById(R.id.spinneraplicacion);
+        completartarea=(Spinner) findViewById(R.id.spinneraplicacion);
+        calculoesfuerzo=(Spinner) findViewById(R.id.spinneraplicacion);
+        efectividadrelativatarea=(Spinner) findViewById(R.id.spinneraplicacion);
+        costototal=(Spinner) findViewById(R.id.spinneraplicacion);
 
 
         siguiente.setOnClickListener(new View.OnClickListener() {
